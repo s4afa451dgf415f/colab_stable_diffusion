1e848f5e832a04f6ffd377250e0b84072f9d94e1
guang <follow@qq.com>
2023-09-16 22:12:26 +0800

    暂时更新

--- main2.py
+++ main2.py
@@ -1,4 +1,158 @@
-import zlib, base64
-exec(zlib.decompress(base64.b64decode('eJztWl9v28gRf/enIPhiufCSoiwlaQA9GLYTHGA7hpMzDjAMYkUupYWWu7zdpSxdEOBQ9Nq7JnfX3ssVaYH2rQUKpEALXItrr58mji9P/QqdpWT9oWT9sxAgRg2DpndnZn+/2dmZJbk0ToTUlsQ8FPEa7f6ntKS83v8vrSVSBESpfkunfyv6d4HgQSol4dqJUp1K0u/RNCZ91UaqKVtLsMSxqj59thYJaWFZtyg3Zh24bZ1698/ur1k0Mh2O0lhqdU51o7CO0PoG9FhN0vFbmKWkChKnjPBe15mjEkZ1Yb26vgFiYMH09aU3qtWSUTf6m139fp9p7qI6hbazardxwN2RKS/YQWi5QFQDS9feVA3CWPWJBMtrMFZP3U6pfVat2pjjhAvy5ovnb37//eVf/nrx6sXlf756++nP3n72pQ0w8rbrFJzIBCdWQ+tE3XddaGmkNScQsdsz5oI3aoygkEZRqqjg6JzUUjoAFQiGa1oIpkbhETYOcPvDJ48Otp98sOPBz8VXf7j84vOW52w5xcLln/9+8erlxnSUqGZ1xS2EFMQLwKpBHAWNSfhHxroBC0XmwFRxvHeHKWiQoJkIyrWfSlZ9au9i2fRVqhrUj2nbUTgiYEAJqez79hWMRlqvA74IBwSwuHHIUEylFNINQR1l6tDSdmEdCdYibowpz/oem64D2oZfvyZpvaGJHBlk097mHd0A6yeVky2/VTmS+nhvPhzAREOsddVRq+IURwHMtLxpBw3KmEg1gPcP6RFMEAkfJN6dB/P6gmPBRYu7kw1tlR7kvTJFMIdNJnX/pDwfjG2OzTS7oDM63PHRQ3RSRgciJAztinPOBA6z1lY5N96RFFo8JNz/qOLcQ0mGDEXgjPkgmOlWWsi45FWKbmasDsbaYMx/FEU0oJj5x4QRrMgoxvkG3rQ5LBoY3362FpLIAjEf1ocf9kgVTLLN8q9vcnjVXBxzKWwsmL6OGh0gwl0VdpcVQBJxohFmDFGOjNaEheaStoEKi1FN0RxdjovhOqDNtAbRMzBvUEjBONFzIxqo3ARKqBmH5T85DSEmAszoJ1ibpk8a/s7hTHjzGroJ6KiZcu5tle64IiE8EYogElII2VnocuI3wbArYsppc1eYMmlSOerPjMZ1kEgYRPwsQNdr3gTbjugITdC2mzJYNxhu00TBBBAE+x6EUy2gkQZm7FkA8xZuAgs8TrEW18XIeeiVjQfqZOY8zrZwE5yQnuAXMkcyWG1bITLBgxYItmm6N4HXwE0BRbqJhxIbExKjGqyyJjSY6jw3uDHNHDTCwwlpOIGtui7YF//428Vnz9++/AXA+/HTb998+91///3S3rzSQYMsvmlf/ukbe6Of8DGEQilYKudH611l2GwBRyg/BEjUESMtwqrE7GYsBJ1ty7tjIZVdm5Z3YD0d3TOdXu1LY1NS7bOzZxYKJ7otE1Du41zQWUhYT3NGnq1Pn9jlkF+3bRJx1MEQqp1YpApWfVYQDolGLQ95vim8/lDhzW1futJ+y/OIr0Kv4tOklPj5aj3DJ4MxjTcm2xwt/u+feyIv6ZIJSaIbK/PQqNn33Uk9LgHmvLMyFw0bvSUOojzBJgGt2EU9s7fESYxyYkrB/5faFCfFTIWrDiNj85a4B55qYsxqmKzaR33Dt8RRV89Eq/bTld1b4iYVSFqD/d+q3XRl97a4idRX7iFSvy3OEZEmYX31MdSzezvcpEr9DQDm8Ai4Ym/lzb/vTouunrU0XWF2mm71XfqJiebOR265jT5kWmLVwDIZ9cVV12PT5cAGbwbnvcfHD7cPXUN4TPVdEguxxopoBTdYhe7OoROo1ii1btt1fOZ6j+jCvTJcu7YWebvTZXvx6sWbz3/9+p/Pf/zhh/nf8QSpZMu90TeaFtoX1vqUOTzZ3nNbmKBIo1gRdK9chB9EYtz76jAcrOvXTYB5i0gZ1R1MzfuwIXtCUhDEbHQ25h1wRhDNTbDJUHQPZUmq5JhvZNcyaeBmKgl1zzGN0qHXopBJyn0O5aAcFStesVysVIJi6ac4rNy9G3j3cODdvVO+U4qKoRdCg+E5PvaqSGUmYQRnCh11zlBPxYgbPCMTMWRjkXg2GJeL5vM6WfL7FE60RTlIM2YxWkv5OeXhPRQSWNWdjz/OwRfKIbxFpeCn9v6uf3S8t/9oe9c+q9qgqwN47mAicJRwyvao8JMH/s7RkX/wwaG//+ihv793srdv1LbscUgqDYUFuBCQ6mNTom1FUZyQuoEZYDBbGtwZvIt42vhrOU8nNFnO0aDYJ9OO4BmNQPG7uqlWi07RKRUX4QAGF6dA2iRItZDV8XMpzpOGJDg8ggWx15MqxLjtnwvZNBArG2tQD5pe9cqGAxRjqgv576NdudJEudxr9a7o1pjoSHbuCpXHhEaCvitUGRManq+NtQmsISXpwmnGbDPDnV23sms5u1bOsnMsEM4J1g2HtKnSqhBNTCQkrpEwhFShsiM53XM9joy1JGQOlbGTNeMfWvLHAsAQx1CpYzxkaXI57ndfe0ZnULHt3lmjHGu7bziUFNLdQWc3+zukmB1FyvOQqsNh64GtmfruT2bsJXJrpLcc1s2nnl/9ERbC6+9/c/m737795YvLlz9/8/U3r//1nTn1ND8VKALLcTDVYzL4QXlZBDyIL4Z8aIKXm4S+/nWTcE0ETecxEuAL0DFf/iYTiZshhb1sMs3Z+5n2El4ww06fx8z0Ag4wFhejPvgSuNxMDvSnM8l/MlyEVV4XGP4PkBEdIQ==')))
-# Created by pyminifier (https://github.com/dzhuang/pyminifier3)
+import random
+import string
+import subprocess
+import sys
+import os
+import concurrent.futures
+import time
+import shutil
 
+params = {}
+for arg in sys.argv[1:]:
+    if arg.startswith('--'):
+        key_value = arg[len('--'):].split('=')
+        if len(key_value) == 2:
+            key, value = key_value
+            params[key] = value
+
+subprocess.run("cd /content/", shell=True)
+if params["ui"] == "anapnoe手机端完美适配":
+    subprocess.run("git clone https://github.com/anapnoe/stable-diffusion-webui /content/colabtools", shell=True)
+elif params["ui"] == "AUTOMATIC1111原版v1.3.0(稳定)":
+    subprocess.run(
+        "git clone -b v1.3.0 --single-branch https://github.com/AUTOMATIC1111/stable-diffusion-webui /content/colabtools", shell=True)
+else:
+    subprocess.run(
+        "git clone -b v1.5.1 --single-branch https://github.com/AUTOMATIC1111/stable-diffusion-webui /content/colabtools", shell=True)
+
+checkpoint_url = {
+    "Dark_sushi_mix.safetensors": "https://huggingface.co/mdl-mirror/dark-sushi-mix/resolve/main/darkSushiMixMix_brighter.safetensors",
+    "AnythingV5V3_v5PrtRE.safetensors": "https://huggingface.co/ckpt/anything-v5.0/resolve/main/AnythingV5V3_v5PrtRE.safetensors",
+    "chilloutmix_NiPrunedFp16Fix.safetensors": "https://huggingface.co/naonovn/chilloutmix_NiPrunedFp32Fix/resolve/main/chilloutmix_NiPrunedFp32Fix.safetensors",
+    "rpg_V4.safetensors": "https://huggingface.co/Anashel/rpg/resolve/main/RPG-V4-Model-Download/RPG-v4.safetensors",
+    "ProtoGen_X5.8-pruned-fp16.safetensors": "https://huggingface.co/darkstorm2150/Protogen_x5.8_Official_Release/resolve/main/ProtoGen_X5.8-pruned-fp16.safetensors",
+    "none": "",
+}
+
+
+def run_git_download():
+    start_time = time.time()
+    subprocess.run(
+        "git clone https://github.com/Physton/sd-webui-prompt-all-in-one /content/colabtools/extensions/sd-webui-prompt-all-in-one", shell=True)
+    subprocess.run(
+        "git clone https://github.com/Mikubill/sd-webui-controlnet /content/colabtools/extensions/sd-webui-controlnet", shell=True)
+    subprocess.run(
+        "git clone https://github.com/dtlnor/stable-diffusion-webui-localization-zh_CN /content/colabtools/extensions/stable-diffusion-webui-localization-zh_CN", shell=True)
+    subprocess.run(
+        "git clone https://github.com/fkunn1326/openpose-editor /content/colabtools/extensions/openpose-editor", shell=True)
+    subprocess.run(
+        "git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete /content/colabtools/extensions/a1111-sd-webui-tagcomplete", shell=True)
+    subprocess.run(
+        "git clone https://github.com/Coyote-A/ultimate-upscale-for-automatic1111 /content/colabtools/extensions/ultimate-upscale", shell=True)
+    subprocess.run(
+        "git clone https://github.com/toriato/stable-diffusion-webui-wd14-tagger /content/colabtools/extensions/stable-diffusion-webui-wd14-tagger", shell=True)
+    subprocess.run(
+        "git clone https://github.com/nonnonstop/sd-webui-3d-open-pose-editor /content/colabtools/extensions/sd-webui-3d-open-pose-editor", shell=True)
+    subprocess.run(
+        "git clone https://github.com/hako-mikan/sd-webui-lora-block-weight /content/colabtools/extensions/sd-webui-lora-block-weight", shell=True)
+    end_time = time.time()
+    print("已克隆git耗时：", end_time - start_time, "秒")
+
+
+def run_aria2c_download():
+    start_time = time.time()
+    subprocess.run(
+        f'aria2c --console-log-level=error -c -x 16 -s 16 -k 1M {checkpoint_url[params["model"]]} -d /content/colabtools/models/Stable-diffusion -o {params["model"]}', shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11e_sd15_ip2p_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11e_sd15_ip2p.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11f1p_sd15_depth_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11f1p_sd15_depth.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_canny_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11p_sd15_canny.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_inpaint_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11p_sd15_inpaint.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_lineart_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11f1p_sd15_depth.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_mlsd_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11p_sd15_mlsd.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_normalbae_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11p_sd15_normalbae.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_openpose_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11p_sd15_openpose.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_scribble_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11p_sd15_scribble.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_seg_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11p_sd15_seg.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15_softedge_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11p_sd15_softedge.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11p_sd15s2_lineart_anime_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11p_sd15s2_lineart_anime.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/comfyanonymous/ControlNet-v1-1_fp16_safetensors/resolve/main/control_v11f1e_sd15_tile_fp16.safetensors -d /content/colabtools/models/ControlNet -o control_v11f1e_sd15_tile_fp16.safetensors", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/lokCX/4x-Ultrasharp/resolve/main/4x-UltraSharp.pth -d /content/colabtools/models/ESRGAN/ -o 4x-UltraSharp.pth", shell=True)
+    subprocess.run(
+        "aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/datasets/daasd/CN.csv/resolve/main/CN.csv -d /content/colabtools/extensions/a1111-sd-webui-tagcomplete/tags -o CN.csv", shell=True)
+    end_time = time.time()
+    print("aria2c完成下载耗时：", end_time - start_time, "秒")
+
+
+def curl_download():
+    start_time = time.time()
+    subprocess.run(
+        "curl -Lo '/content/colabtools/models/VAE/vae-ft-mse-840000-ema-pruned.safetensors' https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors", shell=True)
+    subprocess.run(
+        "curl -Lo '/content/colabtools/models/VAE/kl-f8-anime2.ckpt' https://huggingface.co/hakurei/waifu-diffusion-v1-4/resolve/4c4f05104055c029ad577c18ac176462f0d1d7c1/vae/kl-f8-anime2.ckpt", shell=True)
+    subprocess.run(
+        "curl -Lo '/content/colabtools/models/VAE/animevae.pt' https://huggingface.co/swl-models/animvae/resolve/main/animevae.pt", shell=True)
+    end_time = time.time()
+    print("curl完成下载耗时：", end_time - start_time, "秒")
+
+
+def wget_download():
+    start_time = time.time()
+    subprocess.run("apt install libunwind8-dev -yqq", shell=True)
+    os.environ["LD_PRELOAD"] = "libtcmalloc.so.4"
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+    subprocess.run("sudo apt-get install sox ffmpeg libcairo2 libcairo2-dev", shell=True)
+    end_time = time.time()
+    print("wget完成下载耗时：", end_time - start_time, "秒")
+
+
+def pip_download():
+    start_time = time.time()
+    subprocess.run("pip install xformers xformers==0.0.20", shell=True)
+    end_time = time.time()
+    print("pip完成下载耗时：", end_time - start_time, "秒")
+
+
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
+task1 = executor.submit(run_git_download)
+task2 = executor.submit(run_aria2c_download)
+task3 = executor.submit(curl_download)
+task4 = executor.submit(wget_download)
+task5 = executor.submit(pip_download)
+concurrent.futures.wait([task1, task2, task3, task4, task5])
+
+if os.path.exists(f'/content/colabtools/embeddings'):
+    shutil.rmtree(f'/content/colabtools/embeddings')
+subprocess.run("git clone https://huggingface.co/nolanaatama/embeddings /content/colabtools/embeddings", shell=True)
+
+# 个人插件从云盘的extensions文件夹与VAE文件夹加载
+if params["extensions"]:
+    if os.path.exists("/content/drive/MyDrive/extensions"):
+        subprocess.run("rsync -a /content/drive/MyDrive/extensions/* /content/colabtools/extensions", shell=True)
+        print('已加载云盘里的插件')
+    if os.path.exists("/content/drive/MyDrive/VAE"):
+        subprocess.run("rsync -a /content/drive/MyDrive/VAE/* /content/colabtools/models/VAE", shell=True)
+        print('已加载云盘里的VAE')
+    if os.path.exists("/content/drive/MyDrive/embeddings"):
+        subprocess.run("rsync -a /content/drive/MyDrive/embeddings/* /content/colabtools/embeddings", shell=True)
+        print('已加载云盘里的embeddings')
+    if os.path.exists("/content/drive/MyDrive/lora"):
+        subprocess.run("mkdir -p /content/colabtools/models/Lora")
+        subprocess.run("rsync -a /content/drive/MyDrive/lora/* /content/colabtools/models/Lora", shell=True)
+        print('已加载云盘里的lora')
+    if os.path.exists("/content/drive/MyDrive/checkpoint"):
+        subprocess.run("rsync -a /content/drive/MyDrive/checkpoint/* /content/colabtools/models/Stable-diffusion", shell=True)
+        print('已加载云盘里的Stable-diffusion')
--- main22.py
+++ main22.py
@@ -1,4 +1,287 @@
-import zlib, base64
-exec(zlib.decompress(base64.b64decode('eJy1We1v28YZ/66/4kIMIFlTsh1s+yDkMqCJNwRL1qDJN0UzaPFkc6ZI4UjZ8jQB+9QE2fJSzI3TrUAyLEWKAUW6rmjcuN7+GUl1PvVf6HOvPJKSvbbZF5G8e96f39099yjs9ROaod+lSVwLxTsl6i0dbPRp0iFpqke2BlkYqa8krXVp0kOdJO4MKCVx1ugOsgElKZIUN7co8YPrSRKtDUlnkCVUS9pLa50t0tleD0KKrWWQkYEAeEb+RgYM6XIvCQg8bmT+RkTqQdjtDtIwia1alFD/TLarQGTV+j71eykejWvdhCKfbqIwZrob8LrTWm22mzUUdtlEI818mqW7Ybbl2PW67cIM2iZ76zt+NCAYKFoRieVUu5H2ozBzbGy7QAYS2JymdjE+z9gZvyf49RwbFla1YKyNxWBAuijd9fuOT6kXeZRpz0ivD3ppK2rXkHjyTyo/aRszEs7LQ/kuifyMXEsCJ4kCeHgx2YUHl0X3mEGUQHpiJKaRHwdIkPBXMdqyg2Q3jhI/uBrG23YbY0FSHq8hMuyQfobW+AMSg/wUEaalT8M4c4grvPLjMAt/T9a7IcTI7xFHvTC7+n6WERpjardu3VpuvvUL68LFP7TtmmYLNB+mpAGIdCSLZ6/bnhZVU65V+UR8kh7whRshZG3P6VNySQCH2cCQEUIkGTTymabI6wZgymGzLRsEbNltjgtkjijDrLSE1Pou2RiElmfl6LS8gqxFoqjBsn5rd+lUGcy/gPhRIf3vct8lCPQHMx4meWzWGT/uWiOTWkkdL5eG2QsMWzWGGMHPVqBjSlfMEDZzuLFJYKXwKVdCrsJyDlsWgmVL0DxrXEMrh4JTRVVRqjDXXUrSBhMhlisZZs48r9zWatlmNVW1V4yDvbOP/jl9/3j68N5cw6Vkw3AVbjN+EOaCYyy+YEkhRedwQQSDn1zNnFQ6SIZhmqVOgVNAlZFQEaPCrFcQ6wrSOcHHpsFzqISnuOAHJxPbgABYrmg8+9sXs/2vJoevRgUDxrM7D6d3n1jcEBbQ5mlCJof3pp8+nn70ybdf//n17XsiEdNn/zr54mMuQe5N79xYo5Rt/HJn0uKEEWXLDr6cPnj/m/0nr/c/PHnxoolGkBCQpqzRtsxDxHj68nNh0uRwf3bwdHJ0NDn+AJRMHzwGI6efvTf97/HJ8/cstWAjohfrZbU2y+vycnVNXi6vRwmGKhZKOOAw6CU7xCkm3YxwUfj0ztPXHz4z8qLTspjHTIzgz7OyIClKmCCvilyQFhbFq5dusPhdShiYLyVye7vBTvIrwRCv8OVnfsLsWhywD3Zgc1a3vsrJjHEhjI0raWAP5tQtQ35bySvMCjntXDWb5RJbhjFtpbMwq3kjOGOv+X1WuKDdLciX6dcFrNWoMzyf0lIkKKqVgTLKMwwUEKmcInNJa2ILUCqX8CobMYyQI2JLVYMXtWEIcm/wX9TOiBW/ASXjtqljUezRmREGwM6NgAi7pxOwwPsKmbRKWFvXfhe+34DXCxGFTsXMIm/NJJ7q7xzChbl+0z6fnenv7XeevjNQPodwQabfPMJPz/VZ6JbbsblbIFHPsmKW+vEmcQxDPK1gaVUWB2ZRL60I29VaXzhrjqIwRXGSod8ksTyqlRktk6yNQx6pYHglficKsKThNaHhYFmjx8S6MsaKmanM1ZmncS6mfAxNXt2fHTyHO6E3OkXdGI7n2cuj6d2n0zufTQ7/dHJ8DOf39O7fXz9+Mr1zIIoSJC+bDb/fJ3HglPGSFy0VhLGoSifaFZyJi584xhRVnildmP1ALJYgdyaoTgWliLkNLLZn8Hlam6tpgM325mLPrZWMLR1aJfzO1aPwa4JAQlcj4NXsEeRyH6DAKsRn9wAEohA7M9PVRHPZvBQU5fmco5ift9iYg29xdIclKo7pC3kBUq10q64cPK/6IQomCc5CQanMFeNIjcyFUg5bARLWAUEJOO7INoXFukOs+Lfank3hDpci3mP5JfjGrvD6yowZYYOtqtTRFA3WAXJcMGbhHbzG7TO5oVYl8U5Ik7hl8UkLvBEBEiGrQQht/mrzEigP5sWVZk0lELc0ZiWb/DKVG0VkYVhQdtS3DJMwlclVOnKmWgo56GQkWAfvU9waNtKMhn3HZYge8vaCjChLrKWbSJPD+7YLDmn6Ni9uYYsUl5MULrrFal7W/Hp2fWMvIylWFwDgLPHIZTWAWzPY0bLftj371+znGvv5Ffu5+bbdVugsCb6wunL+p42V0lZSIlrGgipvMwGWSzTNxvnuGI2YGezqcsaFgI/qW5iSyk4AHZ9OuBNmfsjj5ED0WHB6ftbZ4p0U4tPOlkPt1m+X20s/sT0gaASkA9DkgARHOa3RGePfjU2aDPrOiqsSdCu23dZKu3wTtOCaN/v3B5Oj+yf/+cv09qvJ4aezBw+nDx7NHt2eHH3JWgPimsJuXet8RThpMqAdeQGHfYL5UEqq6LE2+E1tEfX3Cx2/b8rzTkSKNZBc0fosYJavJdFdEnsPO+FZx9QkysNVy8UOaIQFY7l0qKZj2WEJecuFjJj8rrz/8g6PmYhVtnn0ArgT+zT0z3dQvQ6LL00iUo+SzXpEdkiECY9DHSaHaPXnqJ7y3220eg2NTC1jVA/QSLWQxxaLgd2wkfS0qJc3dv4falE9QaNC24eSdBBlOO+5N+ggdsBtL90iUYRv0gEBAAeg0CS6fuX6GhtOBll5WCBcyG2IhDHoY7wir4aG29qUpl7ZPA2VNSbFCY2y920C5hy2dWPM5sJYj1oIqzbtTM45zTptB+vQ6R6GaCJbeTCXzUh6xRmtXewh4FrsR8IcPWWW0sa82c0yFiIuq1YMLIv5poiL27chwIhauTVqNnZZS1QEMF/42CRgc//DzlLpMRuTRgmlnF2ypy8/F4WR2VuDGuTbr/9qL+XuGs0j+5vnUEJ9Uq+zjrya97o2lFgjQ9vYdtVtTQeG7zjsANIoZKPNU3zT/xpVPdNTb9YvLrafwMhcD7XWsdz3n31lF7plBRsVdH6siebSwflUDYkSRdWufKef08I02cdCp2jUeScvXs7+8cfZk49H8/Zztox4hVj9a8/p+cP13YRuE5rin7EykciZ0p8snfwfFkXB/vXowVFbOKY8Yfx3GpR+Ng==')))
-# Created by pyminifier (https://github.com/dzhuang/pyminifier3)
+import json
+import re
+import subprocess
+import shutil
+import os
+from concurrent.futures import ThreadPoolExecutor
+import sys
 
+check_dir = "/content/colabtools/models/Stable-diffusion"
+lora_dir = "/content/colabtools/models/Lora"
+
+params = {}
+for arg in sys.argv[1:]:
+    if arg.startswith('--'):
+        key_value = arg[len('--'):].split('=')
+        if len(key_value) == 2:
+            key, value = key_value
+            params[key] = value
+def swap(arr, l, r):
+    temp = arr[l]
+    arr[l] = arr[r]
+    arr[r] = temp
+
+
+def checkRelateMod(oldMod, newMod):
+    try:
+        return oldMod and newMod and oldMod['downloadLink'] == newMod['downloadLink']
+    except Exception as e:
+        print(e)
+
+
+def sanitize_filename(filename):
+    # 定义不兼容字符的正则表达式模式
+    pattern = r'[\\/:*?"<>|]'
+    # 使用下划线替换不兼容字符
+    sanitized_filename = re.sub(pattern, '_', filename)
+    return sanitized_filename
+
+
+def compatibility(preContent):
+    for item in preContent:
+        if bool(item['path']):
+            item['path'] = re.sub("stable-diffusion-webui", "colabtools", item['path'])
+            item['path'] = re.sub(r"colabtools_\w+", "colabtools", item['path'])
+
+
+def dealRelateMod(oldRenameMod, newRenameMod):
+    old_file_path = f"{oldRenameMod['path']}/{oldRenameMod['name']}"
+    # 新模型的定义不带后缀名，所以需要加上处理，且在预处理情况下不知道路径那么只能沿用老模型的路径和名字
+    new_file_dir = (
+        newRenameMod['path']
+        if newRenameMod.get('path') and newRenameMod['path'] != ""
+        else oldRenameMod['path']
+    )
+    new_file_name = (
+        sanitize_filename(newRenameMod['name']) + os.path.splitext(oldRenameMod['name'])[1]
+        if newRenameMod['name'] and newRenameMod['name'] != "未命名"
+        else oldRenameMod['name']
+    )
+    new_file_path = f"{new_file_dir}/{new_file_name}"
+    if old_file_path != new_file_path:
+        try:
+            if os.path.exists(old_file_path):
+                os.rename(old_file_path, new_file_path)
+                oldRenameMod['path'] = new_file_dir
+                oldRenameMod['name'] = new_file_name
+                print(f"{old_file_path}更改为{new_file_path}成功")
+            else:
+                print(f"{old_file_path}不存在，重命名失败")
+        except OSError as e:
+            print(f"更改{old_file_path}时发生错误: {e}")
+    else:
+        print(f"{newRenameMod['name']}已存在且无任何改变，将忽视")
+
+
+def deleteMod(oldDeMod):
+    file_path = f"{oldDeMod['path']}/{oldDeMod['name']}"
+    try:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            # oldCo的行也要进行同步删除
+            print(f"{oldDeMod['name']}删除成功")
+        else:
+            print(f"{oldDeMod['name']}不存在，删除失败")
+    except OSError as e:
+        print(f"删除{oldDeMod['name']}时发生错误: {e}")
+
+
+def LCS(oldCo, newCo):
+    oldStartIdx = 0  # 旧前指针
+    newStartIdx = 0  # 新前指针
+    oldEndIdx = len(oldCo) - 1  # 旧后指针
+    newEndIdx = len(newCo) - 1  # 新后指针
+    oldStartMod = oldCo[oldStartIdx]  # 旧前Mod
+    oldEndMod = oldCo[oldEndIdx]  # 旧后Mod
+    newStartMod = newCo[newStartIdx]  # 新前Mod
+    newEndMod = newCo[newEndIdx]  # 新后Mod
+    linkMap = {}
+    while oldStartIdx <= oldEndIdx and newStartIdx <= newEndIdx:
+        # 新前旧前
+        if checkRelateMod(oldStartMod, newStartMod):
+            dealRelateMod(oldStartMod, newStartMod)
+            oldStartIdx += 1
+            newStartIdx += 1
+            if newStartIdx > newEndIdx or oldStartIdx > oldEndIdx:
+                break
+            oldStartMod = oldCo[oldStartIdx]
+            newStartMod = newCo[newStartIdx]
+        # 新后旧后
+        elif checkRelateMod(oldEndMod, newEndMod):
+            dealRelateMod(oldEndMod, newEndMod)
+            oldEndIdx -= 1
+            newEndIdx -= 1
+            if newStartIdx > newEndIdx or oldStartIdx > oldEndIdx:
+                break
+            oldEndMod = oldCo[oldEndIdx]
+            newEndMod = newCo[newEndIdx]
+        # 新后旧前
+        elif checkRelateMod(oldStartMod, newEndMod):
+            dealRelateMod(oldStartMod, newEndMod)
+            oldStartIdx += 1
+            newEndIdx -= 1
+            if newStartIdx > newEndIdx or oldStartIdx > oldEndIdx:
+                break
+            oldStartMod = oldCo[oldStartIdx]
+            newEndMod = newCo[newEndIdx]
+        # 新前旧后
+        elif checkRelateMod(oldEndMod, newStartMod):
+            dealRelateMod(oldEndMod, newStartMod)
+            oldEndIdx -= 1
+            newStartIdx += 1
+            if newStartIdx > newEndIdx or oldStartIdx > oldEndIdx:
+                break
+            oldEndMod = oldCo[oldEndIdx]
+            newStartMod = newCo[newStartIdx]
+        # 四种均未找到
+        else:
+            # if not linkMap:
+            linkMap = {}
+            # 从 oldStartIdx 开始，到oldEndIdx结束，创建linkMap映射对象
+            for i in range(oldStartIdx, oldEndIdx + 1):
+                downloadLink = oldCo[i]['downloadLink']
+                if downloadLink is not None:
+                    linkMap[downloadLink] = i
+            idxInOld = linkMap.get(newStartMod['downloadLink'], None)
+            if idxInOld is None:
+                print(f"{newStartMod['name']}不存在于旧mod,{newStartMod['downloadLink']}将添加到下载任务队列")
+                content.append(newStartMod)
+
+            else:
+                dealRelateMod(oldCo[idxInOld], newStartMod)
+                swap(oldCo, idxInOld, oldEndIdx)
+                oldEndIdx -= 1
+            # 指针下移，移动新的头
+            newStartIdx += 1
+            if newStartIdx > newEndIdx:
+                break
+            newStartMod = newCo[newStartIdx]
+            oldEndMod = oldCo[oldEndIdx]
+            # oldStartIdx += 1
+    print('new', newStartIdx, newEndIdx)
+    print('old', oldStartIdx, oldEndIdx)
+    # new这里还有剩余Mod没有处理
+    if newStartIdx <= newEndIdx:
+        for i in range(newStartIdx, newEndIdx + 1):
+            print(f"{newCo[i]['name']}为新且不重复mod，将添加到下载任务队列")
+            content.append(newCo[i])
+
+    elif oldStartIdx <= oldEndIdx:
+        i = oldStartIdx
+        while i <= oldEndIdx:
+            if i < len(oldCo):
+                print(f"{oldCo[i]['name']}为旧不重复mod，将删除")
+                deleteMod(oldCo[i])
+                del oldCo[i]
+                oldEndIdx -= 1
+            else:
+                break
+
+
+# LCS(oldArr,oldArr)
+with open(params["json_dir"], 'r') as modelFile:
+    preContent = json.loads(modelFile.read())
+    compatibility(preContent)
+
+oldCo = json.loads(os.environ["oldCo"])
+
+# 更新阶段
+if 'oldCo' and len(oldCo) > 0:
+    content = []
+    print(oldCo)
+    print(preContent)
+    LCS(oldCo, preContent)
+    print(content)
+# 初始化阶段
+else:
+    oldCo = []
+    content = preContent
+
+# 部分下载的情况
+selected_mods = [x.strip() for x in params["name"].split('与') if x.strip()]
+
+
+def get_file_size(file_path):
+    try:
+        file_size_bytes = os.path.getsize(file_path)
+        for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+            if file_size_bytes < 1024.0:
+                break
+            file_size_bytes /= 1024.0
+        return f"{file_size_bytes:.2f} {unit}"
+    except OSError as e:
+        print(f"Error: {e}")
+        return None
+
+
+def get_civitai_file(str):
+    match = re.search(r'[^/]+$', str.decode())
+    if match:
+        return match.group(0).split('\n')[0]
+    else:
+        print("无法从输出中提取文件名")
+
+
+def move_model(source_path, target_path):
+    try:
+        shutil.move(source_path, target_path)
+    except OSError as e:
+        print(f"Error: {e}")
+
+
+def download_file(item):
+    # for item in content:
+    if selected_mods and item['name'] not in selected_mods:
+        return
+    download_url = item['downloadLink']
+    # huggingface
+    match = re.search(r'/([^/]*)$', download_url)
+    file_name = match.group(1)
+    # 开始下载
+    cmd = f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M {download_url} -d {lora_dir}" if '.' not in match.group(
+        1) else f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M {download_url} -d {lora_dir} -o {file_name}"
+    result = subprocess.run(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    if result.returncode == 0:
+        # 判断是civitai还是hugging face还是自定义
+        # civitai
+        if '.' not in file_name:
+            file_name = get_civitai_file(result.stdout)
+        # 如果定义了名字则重命名
+        if item['name'] != '未命名':
+            # 需要进行文件名的兼容处理
+            temp_name = sanitize_filename(item['name']) + os.path.splitext(file_name)[1]
+            os.rename(f"{lora_dir}/{file_name}", f"{lora_dir}/{temp_name}")
+            final_name = temp_name
+        else:
+            final_name = file_name
+        source_path = f"{lora_dir}/{final_name}"
+        file_size = get_file_size(source_path)
+        # 如果path定义了则进行移动
+        if item.get('path') and item['path'] != "":
+            target_path = item['path']
+            move_model(source_path, target_path)
+            item['path'] = target_path
+            print(file_name + '已下载，重命名为：' + final_name)
+            print('移动--', final_name, f'到{target_path}')
+        # 如果为checkpoint则进行移动
+        elif file_size and 'GB' in file_size:
+            move_model(source_path, check_dir)
+            item['path'] = check_dir
+            print(file_name + '已下载，重命名为：' + final_name)
+            print('移动checkpoint--', final_name, f'到{check_dir}文件夹')
+        # 如果为lora则直接调用source_path
+        else:
+            item['path'] = lora_dir
+            print(file_name + '已下载，重命名为：' + final_name)
+        # 改变item['name']用于最小化更新的重命名判断
+        item['name'] = final_name
+        oldCo.append(item)
+    else:
+        print(f"{item['name']}下载失败,请检查{item['downloadLink']}")
+
+
+# download_file(item)
+# 使用线程池进行下载
+with ThreadPoolExecutor(max_workers=5) as executor:
+    for item in content:
+        executor.submit(download_file, item)
